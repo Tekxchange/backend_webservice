7fea6a9465461d3e231584afde4d13260d1fdc3e
Devin Bidwell <dbidwell94@biddydev.com>
2023-03-14 21:01:03 -0700

    log the bad db_url

--- src/db.rs
+++ src/db.rs
@@ -1,29 +1,27 @@
-use rocket::{response::Responder, Response, http::Status};
-use sea_orm::{Database, DatabaseConnection, DbErr};
+use rocket::{http::Status, response::Responder, Response};
+use sea_orm::{ConnectOptions, Database, DatabaseConnection, DbErr};
 use std::env;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum DbError {
     #[error("Unable to connect to database")]
-    ConnectionError(DbErr),
+    ConnectionError(String, DbErr),
     #[error("Environment variables are not set correctly")]
     EnvironmentError,
 }
 
 impl<'r> Responder<'r, 'static> for DbError {
     fn respond_to(self, _: &'r rocket::Request<'_>) -> rocket::response::Result<'static> {
-        Response::build()
-        .status(Status::InternalServerError)
-        .ok()
+        Response::build().status(Status::InternalServerError).ok()
     }
 }
 
 pub async fn establish_connection() -> Result<DatabaseConnection, DbError> {
     let db_url = env::var("DATABASE_URL").or_else(|_| Err(DbError::EnvironmentError))?;
 
-    let res = Database::connect(db_url)
+    let res = Database::connect(db_url.to_owned())
         .await
-        .map_err(|e| DbError::ConnectionError(e))?;
+        .map_err(|e| DbError::ConnectionError(db_url, e))?;
     Ok(res)
 }
