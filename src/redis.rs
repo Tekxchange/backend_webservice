1556e462b2ec37430fbab7026818a33f2d161f7e
Devin Bidwell <dbidwell94@biddydev.com>
2023-05-31 17:03:32 -0700

    wip

--- src/redis.rs
+++ src/redis.rs
@@ -3,13 +3,13 @@ use redis::{aio::Connection, AsyncCommands, FromRedisValue, ToRedisArgs};
 
 #[async_trait]
 pub trait RedisConnection {
-    async fn get_async<'a, K: ToRedisArgs + Send + Sync, RV: FromRedisValue>(
-        &'a mut self,
+    async fn get_async<K: ToRedisArgs + Send + Sync, RV: FromRedisValue>(
+        &mut self,
         key: K,
     ) -> Option<RV>;
 
-    async fn set_async<'a, K: ToRedisArgs + Send + Sync + 'a, V: ToRedisArgs + Send + Sync + 'a>(
-        &'a mut self,
+    async fn set_async<K: ToRedisArgs + Send + Sync, V: ToRedisArgs + Send + Sync>(
+        &mut self,
         key: K,
         value: V,
     ) -> ();
@@ -17,15 +17,15 @@ pub trait RedisConnection {
 
 #[async_trait]
 impl RedisConnection for Connection {
-    async fn get_async<'a, K: ToRedisArgs + Send + Sync, RV: FromRedisValue>(
-        &'a mut self,
+    async fn get_async<K: ToRedisArgs + Send + Sync, RV: FromRedisValue>(
+        &mut self,
         key: K,
     ) -> Option<RV> {
         self.get::<K, RV>(key).await.ok()
     }
 
-    async fn set_async<'a, K: ToRedisArgs + Send + Sync + 'a, V: ToRedisArgs + Send + Sync + 'a>(
-        &'a mut self,
+    async fn set_async<K: ToRedisArgs + Send + Sync, V: ToRedisArgs + Send + Sync>(
+        &mut self,
         key: K,
         value: V,
     ) -> () {
